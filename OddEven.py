f1f3e26a8d292dc723058470266a90b88b1f0c90
Andee12345 <kristine.taborlupa@gmail.com>
2022-03-31 15:59:28 +0800

    practice working

--- OddEven.py
+++ OddEven.py
@@ -1,9 +1,8 @@
-num = int (input ("Enter any number: " )
+num = int (input ("Enter any number: " ))
 
 if (num % 2) == 0:
 
-              print (“The number is even”)
+              print ("The number is even")
 
 else:
-
-              print (“The number is odd”)
+              print ("The number is odd")
